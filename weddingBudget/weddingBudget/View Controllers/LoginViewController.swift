67eb1bfbcdd1367adf826c65e5d0db77e486c507
elizabeth paige <wingateep@Icloud.com>
2020-04-27 18:28:22 -0400

    Login VC

--- weddingBudget/weddingBudget/View Controllers/LoginViewController.swift
+++ weddingBudget/weddingBudget/View Controllers/LoginViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -31,15 +32,24 @@ class LoginViewController: UIViewController {
         
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     @IBAction func loginTapped(_ sender: Any) {
-    }
+    
+        let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+                
+            if error != nil {
+            // Couldn't sign in
+            self.errorLabel.text = error!.localizedDescription
+            self.errorLabel.alpha = 1
+       } else {
+                    
+            let homeViewController = self.storyboard?.instantiateViewController(identifier: Constants.Storyboard.homeViewController) as? HomeViewController
+                    
+            self.view.window?.rootViewController = homeViewController
+            self.view.window?.makeKeyAndVisible()
+        }
+     }
+   }
 }
