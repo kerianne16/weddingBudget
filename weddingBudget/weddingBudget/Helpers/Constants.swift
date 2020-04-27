93573d9a436d652d70c72bb9e11890b66d255ebd
elizabeth paige <wingateep@Icloud.com>
2020-04-27 18:28:48 -0400

    Sign up VC

---
+++ weddingBudget/weddingBudget/Helpers/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  weddingBudget
+//
+//  Created by Elizabeth Wingate on 4/27/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct Storyboard {
+         
+        static let homeViewController = "HomeVC"
+    }
+}
--- weddingBudget/weddingBudget/View Controllers/SignUpViewController.swift
+++ weddingBudget/weddingBudget/View Controllers/SignUpViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import FirebaseAuth
+import Firebase
+import FirebaseFirestore
 
 class SignUpViewController: UIViewController {
 
@@ -24,15 +27,6 @@ class SignUpViewController: UIViewController {
     @IBOutlet weak var signUpButton: UIButton!
     @IBOutlet weak var errorLabel: UILabel!
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
     func setUpElements() {
         
         errorLabel.alpha = 0
@@ -44,7 +38,78 @@ class SignUpViewController: UIViewController {
         Utilities.styleFilledButton(signUpButton)
     }
     
-    @IBAction func signUpTapped(_ sender: Any) {
+    func validateFields() -> String? {
+        
+     if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+        lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+        emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+        passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            
+        return "Please fill in all fields."
+  }
+        
+        let cleanedPassword = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        
+        if Utilities.isPasswordValid(cleanedPassword) == false {
+            // Password isn't secure enough
+            return "Please make sure your password is at least 8 characters, contains a special character and a number."
+        }
+        return nil
     }
     
+    @IBAction func signUpTapped(_ sender: Any) {
+    let error = validateFields()
+            
+    if error != nil {
+  
+    showError(error!)
+        
+    } else {
+             
+       let firstName = firstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+       let lastName = lastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+       let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+       let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+                
+  
+       Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
+                    
+        // Check for errors
+        if err != nil {
+                        
+        self.showError("Error creating user")
+        
+        } else {
+            
+            let db = Firestore.firestore()
+                            
+            db.collection("users").addDocument(data: ["firstname":firstName, "lastname":lastName, "uid": result!.user.uid ]) { (error) in
+                                
+            if error != nil {
+                                    // Show error message
+            self.showError("Error saving user data")
+        }
+}
+                            
+          self.transitionToHome()
+    }
+}
+                    
+    }
+}
+    func showError(_ message:String) {
+                
+        errorLabel.text = message
+        errorLabel.alpha = 1
+    }
+            
+    func transitionToHome() {
+                
+        let homeViewController = storyboard?.instantiateViewController(identifier:
+            Constants.Storyboard.homeViewController) as? HomeViewController
+                
+        view.window?.rootViewController = homeViewController
+        view.window?.makeKeyAndVisible()
+                
+    }
 }
