8c4b75e1e5eecf601026ed9caa64d94980400a98
Keri Levesque <kerianne166@gmail.com>
2020-04-27 11:35:53 -0400

    added starter project (swift)

---
+++ weddingBudget/weddingBudget/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  weddingBudget
+//
+//  Created by Keri Levesque on 4/27/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
