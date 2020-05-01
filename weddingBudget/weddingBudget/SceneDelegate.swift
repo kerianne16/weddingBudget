7084802bde362c94d671f29ccd7bff03f2c3ed21
Keri Levesque <kerianne166@gmail.com>
2020-04-30 20:42:40 -0400

    checklist complete

--- weddingBudget/weddingBudget/SceneDelegate.swift
+++ weddingBudget/weddingBudget/SceneDelegate.swift
@@ -7,7 +7,7 @@
 //
 
 import UIKit
-
+import SwiftUI
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
@@ -17,7 +17,21 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+      if let windowScene = scene as? UIWindowScene {
+                let window = UIWindow(windowScene: windowScene)
+                
+                // Get the managedObjectContext from the persistent container
+                let managedObjectContext = (UIApplication.shared.delegate as? AppDelegate)!.persistentContainer.viewContext
+                
+                // Pass it to the ContentView through the managedObjectContext @Environment variable
+                let homeTasksView = HomeTasksView()
+                    .environment(\.managedObjectContext, managedObjectContext)
+                
+                window.rootViewController = UIHostingController(rootView: homeTasksView)
+                
+                self.window = window
+                window.makeKeyAndVisible()
+            }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -46,6 +60,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
+         (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
